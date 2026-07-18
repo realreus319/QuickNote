c0de047de5fe6f81de85ce376002e6b424202999
realreus319 <108124249+realreus319@users.noreply.github.com>
2026-07-19 01:14:03 +0800

    fix(build): import Geist variable weight stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import '@fontsource-variable/geist/index.css'
+import '@fontsource-variable/geist/wght.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
